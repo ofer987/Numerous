1eb8ebb132a950047a87d8610a6f2714343930b5
ofer987 <ofer987@gmail.com>
2013-09-09 00:50:06 -0400

    - Fixed test case: Time comparison should be done in UTC

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -48,16 +48,16 @@ class ArticleTest < ActiveSupport::TestCase
     new_article = Article.new(gazette_id: gazettes(:peru_stories).id, published_at: DateTime.new(2012, 5, 12).getutc)
     
     assert new_article.save, "should be able to modify an article's published_at datetime"
-    assert new_article.published_at == DateTime.new(2012, 5, 12).getutc, "the article cannot save its selected datetime"
+    assert new_article.published_at.getutc == DateTime.new(2012, 5, 12).getutc, "the article cannot save its selected datetime"
   end
 
   test "article's default time published_at is now" do
     now_expected = DateTime.now.getutc
     new_article = Article.new(gazette_id: gazettes(:peru_stories).id)
 
-    assert new_article.published_at.year == now_expected.year &&
-               new_article.published_at.month == now_expected.month &&
-               new_article.published_at.day == now_expected.day
+    assert new_article.published_at.getutc.year == now_expected.year &&
+               new_article.published_at.getutc.month == now_expected.month &&
+               new_article.published_at.getutc.day == now_expected.day
 
     assert new_article.valid?
   end
