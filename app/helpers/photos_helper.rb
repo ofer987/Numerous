104b9966a173ca798f7d68b056db6bcd6d8d52f0
ofer987 <ofer987@gmail.com>
2013-11-13 22:21:39 -0500

    Photos should not be displayed using asset pipeline

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,4 +1,9 @@
 module PhotosHelper
+  def fichier_path(photo_filename)
+    #asset_path(photo_filename) || photo_filename
+    photo_filename
+  end
+  
   def photos_list(tags=nil)
     html = <<HTML 
       <div class="photos-list">
