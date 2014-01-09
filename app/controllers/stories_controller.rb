bf0c92598fa6ba730a29f260a59cfbe1247be9e4
ofer987 <ofer987@gmail.com>
2014-01-08 21:54:53 -0500

    Fixed a bug in the Stories#create_photo controller action that it did not create the article photo relationhip

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -21,9 +21,9 @@ class StoriesController < ApplicationController
   def create_photo
     @article = Article.find_by_id(params[:article_id])
   
-    @photo = @article.photos.new(load_photo_file: photo_params[:load_photo_file])
-    if @photo and @photo.valid?
-      @photo.save
+    @photo = Photo.new(load_photo_file: photo_params[:load_photo_file])
+    if @photo.valid?
+      @article.photos << @photo
       render file: 'stories/create_photo.js'
     else
       render file: 'stories/upload_file_error.js', locals: { notice: "Error uploading file" }
