1bd3a3c4c3459408e66c9bf45c4219b895c3e314
ofer987 <ofer987@gmail.com>
2014-02-20 18:53:06 -0500

    Added Gym as PlaceTypes

--- db/migrate/20140220235026_add_gym_to_place_type.rb
+++ db/migrate/20140220235026_add_gym_to_place_type.rb
@@ -1,11 +1,9 @@
 class AddGymToPlaceType < ActiveRecord::Migration
-  def change
   def up
     PlaceType.create!(name: 'Gym')
   end
 
   def down
     PlaceType.where(name: 'Gym').destroy_all
-  end
   end
 end
