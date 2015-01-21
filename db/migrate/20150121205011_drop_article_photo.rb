2f6f8825b80b99e567b4ef5698f2ea9fc11c3dd6
Dan Jakob Ofer <ofer987@gmail.com>
2015-01-21 17:01:31 -0500

    Article to Photo relationship is a one-to-many instead of many-to-many, which is unneccessary according to usage experience.

---
+++ db/migrate/20150121205011_drop_article_photo.rb
@@ -0,0 +1,33 @@
+class DropArticlePhoto < ActiveRecord::Migration
+  def up
+    add_column :photos, :article_id, :int, null: true
+
+    ArticlePhoto.all.each do |article_photo|
+      execute <<-SQL
+        UPDATE photos
+        SET article_id = #{article_photo.article_id}
+        WHERE id = #{article_photo.photo_id}
+      SQL
+    end
+
+    drop_table :article_photos
+  end
+
+  def down
+    create_table :article_photos do |t|
+      t.integer :article_id
+      t.integer :photo_id
+
+      t.timestamps
+    end
+
+    execute <<-SQL
+      INSERT INTO article_photos('article_id', 'photo_id')
+      SELECT photos.article_id, photos.id
+      FROM photos
+      WHERE article_id IS NOT NULL
+    SQL
+
+    remove_column :photos, :article_id
+  end
+end
--- test/models/article_photo_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ArticlePhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
