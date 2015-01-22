d9719447e768563a076558eeac0f01b5bc60902d
Dan Jakob Ofer <ofer987@gmail.com>
2015-01-22 17:57:30 -0500

    use the new billet class instead of article.

--- test/controllers/billets_controller_test.rb
+++ test/controllers/billets_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
+require 'test_fileable'
 
 class BilletsControllerTest < ActionController::TestCase
+  include TestFileable
+
   setup do
-    @billet = billets(:one)
+    @cusco_trip_billet = billets(:cusco_trip)
+
+    @all_billet_photos_attributes = Hash.new
+    Photo.all.each_with_index do |photo, index|
+      @all_billet_photos_attributes["#{index}"] =
+        { is_selected: "0", id: "#{photo.id}" }
+    end
   end
 
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:billets)
   end
 
   test "should get new" do
@@ -16,34 +24,146 @@ class BilletsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should get edit' do
+    get :edit, id: @cusco_trip_billet.to_param
+    assert_response :success
+  end
+
   test "should create billet" do
+    billet = {
+      content: 'new story',
+      published_at: DateTime.now
+    }
+
     assert_difference('Billet.count') do
-      post :create, billet: {  }
+      post :create, format: :js, billet: billet
     end
 
     assert_redirected_to billet_path(assigns(:billet))
   end
 
-  test "should show billet" do
-    get :show, id: @billet
-    assert_response :success
-  end
+  test "should create billet and add two photos" do
+    new_billet = {
+      content: 'new story',
+      billet_photos_attributes: @all_billet_photos_attributes
+    }
+    new_billet[:billet_photos_attributes].each do |index, billet_photo|
+      billet_photo[:is_selected] = "1" if billet_photo[:id].to_i == photos(:eaton_college).id || billet_photo[:id].to_i == photos(:nobody_commented).id
+    end
 
-  test "should get edit" do
-    get :edit, id: @billet
-    assert_response :success
+    pre_billet_photos_count = BilletPhoto.count
+    assert_difference('Billet.count') do
+      post :create, billet: new_billet, is_convert_to_html: false
+    end
+    post_billet_photos_count = BilletPhoto.count
+
+    assert_equal(
+      pre_billet_photos_count + 2,
+      post_billet_photos_count,
+      "Did not add the two photos to the new billet")
+    assert_redirected_to billet_path(assigns(:billet))
   end
 
   test "should update billet" do
-    patch :update, id: @billet, billet: {  }
-    assert_redirected_to billet_path(assigns(:billet))
+    new_content = 'This is an awesome story'
+
+    put :update, format: :js, id: @cusco_trip_billet,
+      billet: { content: new_content }
+    assert_response :success
+    assert_equal(
+      "<p>#{new_content}</p>",
+      assigns(:billet).content,
+      "The billet's content should have been updated")
   end
 
-  test "should destroy billet" do
+  test "should fail to update billet" do
+    put :update, format: :js, id: @cusco_trip_billet,
+      billet: { published_at: nil }
+    assert_response :success
+    refute assigns(:billet).valid?,
+      "The billet should not have been updated"
+  end
+
+  test "should delete billet" do
     assert_difference('Billet.count', -1) do
-      delete :destroy, id: @billet
+      delete :destroy, id: @cusco_trip_billet.to_param
+    end
+    assert_redirected_to billets_url
+  end
+
+  test "should be able to modify an billet's published_at date" do
+    modified_date = DateTime.new(2010, 5, 12)
+
+    @cusco_trip_billet.published_at = modified_date
+
+    post :update, format: :js, id: @cusco_trip_billet, billet: { published_at: modified_date }
+    assert assigns(:billet).published_at == modified_date,
+      "The billet should be able to modify its published_at datetime"
+  end
+
+  test "should add a photo to an billet" do
+    billet = billets(:cusco_trip)
+
+    # Add the photo package
+    photo = photos(:package)
+    params =
+      {
+        id: billet.id,
+        billet_photos_attributes: @all_billet_photos_attributes
+      }
+    params[:billet_photos_attributes].each do |key, value|
+      value[:is_selected] = "1" if value[:id].to_i == photo.id
     end
 
-    assert_redirected_to billets_path
+    # the photo should have these expected tags after the update, including its current photos
+    expected_photos = []
+    expected_photos << photo
+
+    billet.photos.each do |existing_photo|
+      expected_photos << existing_photo
+      params[:billet_photos_attributes].each do |key, value|
+        value[:is_selected] = "1" if value[:id].to_i == existing_photo.id
+      end
+    end
+
+    # update the billet: add the new photo
+    put :update, format: :js, billet: params, id: billet.id, is_convert_to_html: false
+    assert_equal(
+      expected_photos.count,
+      billet.billet_photos.count,
+      "The new photo was not added. Errors: #{billet.errors.full_messages}")
+
+    # Does the billet have all the expected_photos?
+    expected_photos.each do |p|
+      assert billet.billet_photos.any? { |verify_billet_photo|
+        verify_billet_photo.photo_id == p.id
+      }, "The billet is missing the photo #{p.title}"
+    end
+  end
+
+  test "should post new photo to existing billet" do
+    existing_billet = billets(:cusco_trip)
+
+    assert_difference('Photo.count', 1) do
+      post :create_photo,
+        format: :js,
+        billet_id: existing_billet.id,
+        photo: { load_photo_file: self.photo_data }
+    end
+    assert_response :success
+  end
+
+  test 'should create billet and assign it tags' do
+    billet = {
+      title: 'new billet',
+      content: 'Billet with tags',
+      tags_attributes: 'england, quebec'
+    }
+
+    assert_difference('Billet.count', 1) do
+      post :create, format: :js, billet: billet
+    end
+    assert assigns(:billet).tags.count == 2,
+      'the two tags were not added to the billet'
   end
 end
--- test/models/billet_test.rb
+++ test/models/billet_test.rb
@@ -1,7 +1,79 @@
 require 'test_helper'
 
 class BilletTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @valid_billet = Billet.new do |billet|
+      billet.title = "Interesting Story"
+      billet.sub_title = "You Should Read This!"
+      billet.content = "Lots of interesting things to read here."
+      billet.published_at = DateTime.now
+    end
+  end
+
+  test "should be able to modify an billet's published_at date" do
+    new_billet = Billet.new(published_at: DateTime.new(2012, 5, 12).getutc)
+
+    assert new_billet.save, "should be able to modify an billet's published_at datetime"
+    assert new_billet.published_at.getutc == DateTime.new(2012, 5, 12).getutc, "the billet cannot save its selected datetime"
+  end
+
+  test "billet's default time published_at is now" do
+    now_expected = DateTime.now.getutc
+    new_billet = Billet.new
+
+    assert new_billet.published_at.getutc.year == now_expected.year &&
+               new_billet.published_at.getutc.month == now_expected.month &&
+               new_billet.published_at.getutc.day == now_expected.day
+
+    assert new_billet.valid?
+  end
+
+  test 'should save billet' do
+    assert @valid_billet.save
+  end
+
+  test 'billet should have tags' do
+    billet = billets(:cusco_trip)
+    assert billet.tags.count == 1
+
+    billet.tags << Tag['england']
+    billet.tags << Tag.find_or_init_by_name('toronto')
+    billet.save!
+
+    assert billet.tags.count == 3
+    assert billet.tags.find_by_name('england')
+    assert billet.tags.find_by_name('toronto')
+  end
+
+  test 'should add tags when setting the tags_attributes property' do
+    billet = Billet.new(title: 'new billet',
+                          content: 'Hello from Canada')
+    billet.tags_attributes =
+      { tags_attributes: 'toronto , montreal, quebec city' }
+    assert billet.tags.to_a[0].name == 'toronto'
+    assert billet.tags.to_a[1].name == 'montreal'
+    assert billet.tags.to_a[2].name == 'quebec city'
+  end
+
+  test 'should convert tag names to lower case' do
+    billet = Billet.new
+    billet.tags_attributes = { tags_attributes: 'Toronto' }
+    assert billet.tags.to_a[0].name == 'toronto'
+  end
+
+  test 'billet content should be displayed in paragraph form' do
+    billet = Billet.new(content: "Hello\nWorld")
+
+    assert "<p>Hello<br />\nWorld</p>" == billet.content
+  end
+
+  test 'deleting an billet should set associated photos to billet_id = nil' do
+    billet = billets(:cusco_trip)
+
+    assert billet.photos.count > 0, 'billet should have photos in order to perform test'
+    photos = billet.photos
+
+    billet.destroy
+    assert photos.count > 0, 'photos should not have been deleted'
+  end
 end
