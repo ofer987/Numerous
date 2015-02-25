cbb7802da9dcfdf4a2580a9f9420d1843888d967
Dan Jakob Ofer <ofer987@gmail.com>
2015-02-24 20:28:09 -0500

    Removed Contact and ContactType.

--- app/models/contact_type.rb
+++
@@ -1,8 +0,0 @@
-class ContactType < ActiveRecord::Base
-  # id: primary key, not nil
-  # name: nvarchar(255), not nil
-  # description: text
-
-  validates :name, presence: { allow_blank: false, allow_nil: false }
-  validates :description, presence: { allow_blank: false, allow_nil: false }
-end
