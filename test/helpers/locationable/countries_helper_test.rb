6f5030ba3db26f7cdaf016ad4dca7f688abe9a95
Dan Jakob Ofer <ofer987@gmail.com>
2015-02-24 20:09:21 -0500

    Deleted lots of locationable, destinations, countries, and cities tests. And deleted all related code to the websites controller and model

--- test/helpers/locationable/countries_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Locationable::CountriesHelperTest < ActionView::TestCase
-end
--- test/helpers/locationable/places_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Locationable::PlacesHelperTest < ActionView::TestCase
-end
