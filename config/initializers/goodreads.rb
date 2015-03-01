a0fab72f7de8333b88b786d4bb3aad5af06c74f2
Dan Jakob Ofer <ofer987@gmail.com>
2015-03-01 15:20:56 -0500

    Remove the unused books controller that displays the books in my personal bookshelves on my personal Goodreads account.

--- config/initializers/goodreads.rb
+++
@@ -1,3 +0,0 @@
-Goodreads.configure(
-  api_key: 1234567, #your API key, get it on http://www.goodreads.com/api/keys
-  api_secret: 0123456789ABCDEF, #your API secret, http://www.goodreads.com/api/keys)
